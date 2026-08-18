ae52ab0b54345e5117fff71fe82d2e5077d71e42
TshepoM-ux <motlohitshepo36@gmail.com>
2026-08-18 11:53:02 -0400

    commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -6,4 +7,22 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = header )
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions.get("anger", 0)
+    disgust_score = emotions.get("disgust", 0)
+    fear_score = emotions.get("fear", 0)
+    joy_score = emotions.get("joy", 0)
+    sadness_score = emotions.get("sadness", 0)
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion,
+    }
